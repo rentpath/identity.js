447aedfaeecc19de80b24b4753243fd2f07237ff
Martin Streicher <martin.streicher@gmail.com>
2015-10-20 15:30:06 -0400

    [MP/MS][105588386] Fix bad path and port.

--- src/zutron.js
+++ src/zutron.js
@@ -1,14 +1,15 @@
 const UniversalZid = {
-  fetch: function (host = 'http://zutron.primedia.com', port = 80, target = '/universal_zids') {
+  fetch: function (host = 'http://zutron.primedia.com', port = 80, target = '/universal_zids/new') {
     var path    = target.replace(/^\//, '');
     var url     = `${host}:${port}/${path}`;
     var request = new XMLHttpRequest();
 
-    request.open('POST', url, true);
+    request.open('GET', url, true);
 
     request.onload = function() {
       if (request.status >= 200 && request.status < 400) {
         var data = JSON.parse(request.responseText);
+        console.log(data);
       } else {
       }
     };
