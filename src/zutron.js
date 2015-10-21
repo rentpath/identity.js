51c23236109c14e5aef8daaecad9d8ceac2c7f93
Martin Streicher <martin.streicher@gmail.com>
2015-10-21 13:14:59 -0400

    [MP/MS][105588386] Allow caller to use async. Add retry ability.

--- src/zutron.js
+++ src/zutron.js
@@ -1,24 +1,71 @@
 const UniversalZid = {
-  fetch: function (host = 'http://zutron.primedia.com', port = 80, target = '/universal_zids/new') {
-    var path    = target.replace(/^\//, '');
-    var url     = `${host}:${port}/${path}`;
-    var request = new XMLHttpRequest();
+  fetch:
+    function (
+      successFn,
+      errorFn,
+      retries = 3,
+      timeout = 500,
+      host    = 'http://zutron.primedia.com',
+      port    = 80,
+      target  = '/universal_zids/new')
+    {
+      let path    = target.replace(/^\//, '');
+      let url     = `${host}:${port}/${path}`;
+      let request = new XMLHttpRequest();
 
-    request.open('GET', url, true);
+      request.open('POST', url, true);
+      request.setRequestHeader('Accept', 'application/json');
+      request.setRequestHeader('Content-Type', 'application/json');
 
-    request.onload = function() {
-      if (request.status >= 200 && request.status < 400) {
-        var data = JSON.parse(request.responseText);
-        console.log(data);
+      request.onload =
+        function () {
+          if (request.status >= 200 && request.status < 400) {
+            let data = JSON.parse(request.responseText);
+            successFn.call(data);
+          } else {
+            if (errorFn) {
+              errorFn(this.status, this.responseText);
+            }
+          }
+        };
+
+      request.onerror =
+        function () {
+          if (this.readyState === 4 && this.status === 0) {
+            errorFn('NO_NETWORK');
+          } else {
+            errorFn(this.status, this.responseText);
+          }
+        };
+
+      if (retries > 0) {
+        request.ontimeout = function () {
+          fetch(successFn, errorFn, retries - 1, timeout, host, port, target);
+        };
       } else {
+        request.ontimeout = function () {
+          errorFn('timeout');
+        };
       }
-    };
 
-    request.onerror = function() {
-    };
+      request.timeout = timeout;
+      request.send();
+    },
 
-    request.send();
-  }
+  push:
+    function (params)
+    {
+      let methodName = params.shift();
+      this[methodName].apply(this, params);
+    }
 };
 
 export default UniversalZid;
+
+if (window && window.UniversalZid) {
+  while (window.UniversalZid.length) {
+    UniversalZid.push(window.UniversalZid.shift());
+  }
+}
+
+window.UniversalZid = UniversalZid;
