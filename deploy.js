7ae4f04736214515b21db48529e9489576ceee96
Martin Streicher <martin.streicher@gmail.com>
2016-01-08 10:39:54 -0500

    [MS][00000000] Deploy only the current version of files; not final deploy.js, but has spirit

--- deploy.js
+++ deploy.js
@@ -1,29 +1,35 @@
 #! /usr/bin/env node
 
-var bucket  = process.env['AWS_BUCKET_NAME'];
+var access  = process.env['AWS_ACCESS_KEY']  || process.env['RENTPATHCODE_BUCKET_ACCESS_KEY'];
+var bucket  = process.env['AWS_BUCKET_NAME'] || process.env['RENTPATHCODE_BUCKET_NAME'];;
 var exec    = require('child_process').exec;
 var file    = require('file');
 var pack    = require('./package.json');
 var path    = require('path');
+var prefix  = './node_modules/.bin/s3-cli put ./dist/'
+var secret  = process.env['AWS_SECRET_KEY']  || process.env['RENTPATHCODE_BUCKET_SECRET_KEY'];;
 var target  = pack.name;
 var version = pack.version;
 
-if ((typeof bucket == 'undefined') ||
-    (typeof process.env['AWS_ACCESS_KEY'] == 'undefined') ||
-     (typeof process.env['AWS_SECRET_KEY'] == 'undefined')) {
+if ((typeof bucket == 'undefined') || (typeof access == 'undefined') ||
+    (typeof secret == 'undefined')) {
   console.log('Error: AWS environment variables are not set;');
-  console.log('       Check AWS_ACCESS_KEY, AWS_BUCKET_NAME, and AWS_SECRET_KEY.');
+  console.log('  Check AWS_ACCESS_KEY, AWS_BUCKET_NAME, and AWS_SECRET_KEY.');
+  console.log('  and/or RENTPATHCODE_BUCKET_ACCESS_KEY, RENTPATHCODE_BUCKET_NAME, and RENTPATHCODE_BUCKET_SECRET_KEY.');
   process.exit();
 }
 
 file.walkSync('./dist', function(dirPath, dirs, files) {
   files.forEach(function(file) {
-    var cleanName   = file.replace(/\-[\d\.]+(?=\.)/, '');
-    var commandLine = './node_modules/.bin/s3-cli put ./dist/' + file + ' ' +
-      's3://' + path.join(bucket, '/', target, version, cleanName);
+    if (file.match(version)) {
+      var cleanName   = file.replace(/\-[\d\.]+(?=\.)/, '');
+      var bucketPath  = 's3://' + path.join(bucket, '/', target, version, cleanName)
+      var commandLine = prefix + file + ' ' + bucketPath;
 
-    console.log('Executing: ' + commandLine);
-
-    exec(commandLine, function(err, stdout, stderr) { console.log(stdout); console.log(stderr); });
+      console.log('Executing: ' + commandLine);
+      exec(commandLine, function(err, stdout, stderr) {
+        console.log(stderr || stdout);
+      });
+    }
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,15 +1,1 @@
 import Identity from './identity';
-
-const host = 'http://identity.rentpathservices.com';
-const port = 80;
-
-function report() {
-  const id = this.universal_zid.uuid;
-
-  console.log('Reporting: ');
-  console.log(id);
-  Identity.cookify(id);
-  Identity.track(() => { console.log('Success!'); }, () => {}, host, port);
-  console.log(Identity.uzid()); }
-
-Identity.fetch(report, () => {}, host, port);
