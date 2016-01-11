62fac7113405018857f4f6910fa4328abc398800
Martin Streicher <martin.streicher@gmail.com>
2016-01-11 12:14:48 -0500

    Fix path of JS file in bucket. Use proper env names.

--- deploy.js
+++ deploy.js
@@ -1,21 +1,23 @@
 #! /usr/bin/env node
 
-var access  = process.env['AWS_ACCESS_KEY']  || process.env['RENTPATHCODE_BUCKET_ACCESS_KEY'];
-var bucket  = process.env['AWS_BUCKET_NAME'] || process.env['RENTPATHCODE_BUCKET_NAME'];;
+var access  = process.env['AWS_ACCESS_KEY']  || process.env['SERVICES_RENTPATHCODE_BUCKET_ACCESS_KEY'];
+var bucket  = process.env['AWS_BUCKET_NAME'] || process.env['SERVICES_RENTPATHCODE_BUCKET_NAME'];
 var exec    = require('child_process').exec;
 var file    = require('file');
 var pack    = require('./package.json');
 var path    = require('path');
-var prefix  = './node_modules/.bin/s3-cli put ./dist/'
-var secret  = process.env['AWS_SECRET_KEY']  || process.env['RENTPATHCODE_BUCKET_SECRET_KEY'];;
+var prefix  = './node_modules/.bin/s3-cli put ./dist/';
+var secret  = process.env['AWS_SECRET_KEY']  || process.env['SERVICES_RENTPATHCODE_BUCKET_SECRET_KEY'];
 var target  = pack.name;
 var version = pack.version;
 
 if ((typeof bucket == 'undefined') || (typeof access == 'undefined') ||
     (typeof secret == 'undefined')) {
   console.log('Error: AWS environment variables are not set;');
-  console.log('  Check AWS_ACCESS_KEY, AWS_BUCKET_NAME, and AWS_SECRET_KEY.');
-  console.log('  and/or RENTPATHCODE_BUCKET_ACCESS_KEY, RENTPATHCODE_BUCKET_NAME, and RENTPATHCODE_BUCKET_SECRET_KEY.');
+  console.log('  Check local AWS_ACCESS_KEY, AWS_BUCKET_NAME, and AWS_SECRET_KEY,');
+  console.log('  and/or rpenv SERVICES_RENTPATHCODE_BUCKET_ACCESS_KEY,');
+  console.log('  SERVICES_RENTPATHCODE_BUCKET_NAME, and ');
+  console.log('  SERVICES_RENTPATHCODE_BUCKET_SECRET_KEY.');
   process.exit();
 }
 
